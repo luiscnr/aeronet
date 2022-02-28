b7bb1445e254db6fa043af8a28ca7a2787ec8c57
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2022-02-28 16:23:51 +0100

    Commit 28/02/2022 16:25 (work with zip files

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 import tarfile
-
 import requests
 
 parser = argparse.ArgumentParser(
--- trims3images.py
+++ trims3images.py
@@ -6,9 +6,12 @@ import numpy as np
 from netCDF4 import Dataset
 
 import s3olcitrim_bal_frompy as trimtool
+import zipfile as zp
 
-parser = argparse.ArgumentParser(
-    description="Search and trim S3 Level 1B products around a in-situ location")
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+parser = argparse.ArgumentParser(description="Search and trim S3 Level 1B products around a in-situ location")
 parser.add_argument('-site', "--site",
                     help="Aeronet site (Baltic: Gustav_Dalen_Tower, Helsinki_Lighthouse, Irbe_Lighthouse")
 parser.add_argument('-ilat', "--insitu_lat", help="In situ lat")
@@ -16,7 +19,8 @@ parser.add_argument('-ilon', "--insitu_lon", help="In situ lon")
 parser.add_argument('-anc', "--anet_nc_file", help="Aeronet NC File")
 parser.add_argument('-fsit', "--sites_file", help="Sites File")
 parser.add_argument('-s', "--sourcedir", help="Output directory")
-parser.add_argument('-o', "--outputdir", help="Output directory", required=False)
+parser.add_argument('-o', "--outputdir", help="Output directory", required=True)
+parser.add_argument('-z', "--unzip_path", help="Temporal unzip directory")
 parser.add_argument('-sd', "--startdate", help="The Start Date - format YYYY-MM-DD ")
 parser.add_argument('-ed', "--enddate", help="The End Date - format YYYY-MM-DD ")
 parser.add_argument("-l", "--createlist", help="Create list", action="store_true")
@@ -37,6 +41,7 @@ def main():
     ANET_SOURCE_DIR = '/store3/HYPERNETS/INSITU_AOC/NC/'
     source_dir = '/dst04-data1/OC/OLCI/trimmed_sources'
     file_sites = '/store3/HYPERNETS/INSITU_AOC/site_list.ini'
+    unzip_path = '/home/Luis.Gonzalezvilas/TEMPDATA/unzip_folder'
     if args.sites_file:
         file_sites = args.sites_file
     site = None
@@ -60,6 +65,7 @@ def main():
         insitu_lon = float(options[site]['Longitude'])
         if args.verbose:
             print(f'SITE: {site} latitude:{insitu_lat}, longitude:{insitu_lon}')
+    point_site = Point(insitu_lon, insitu_lat)
     w = insitu_lon - 0.15
     e = insitu_lon + 0.15
     s = insitu_lat - 0.15
@@ -77,6 +83,10 @@ def main():
         os.mkdir(out_dir_site)
     if args.verbose:
         print(f'Output directory: {out_dir_site}')
+
+    if args.unzip_path:
+        unzip_path = args.unzip_path
+        print(f'Unzip temporal path: {unzip_path}')
 
     file_nc = None
     if args.anet_nc_file:
@@ -114,12 +124,19 @@ def main():
     date_list = areader.get_available_dates(start_date, end_date)
     res_list = []
     for d in date_list:
+        if args.verbose:
+            print()
+            print(f'DATE: {d}')
+            print()
         year = d.strftime('%Y')
         jday = d.strftime('%j')
         source_dir_date = os.path.join(source_dir, year, jday)
         if os.path.exists(source_dir_date):
             for prod in os.listdir(source_dir_date):
                 path_prod = os.path.join(source_dir_date, prod)
+                iszipped = False
+                if args.verbose:
+                    print(f'PRODUCT: {path_prod}')
                 flag_location = -1
                 if prod.endswith('SEN3') and prod.find('EFR') > 0 and prod.find('BAL') > 0 and os.path.isdir(path_prod):
                     path_geo = os.path.join(path_prod, 'geo_coordinates.nc')
@@ -129,20 +146,55 @@ def main():
                         lonArray = dset.variables['longitude'][:, :]
                         flag_location = check_location(latArray, lonArray, insitu_lat, insitu_lon)
 
+                if prod.endswith('.zip') and prod.find('EFR') > 0 and zp.is_zipfile(path_prod):
+                    iszipped = True
+                    with zp.ZipFile(path_prod, 'r') as zprod:
+                        fname = path_prod.split('/')[-1][0:-3] + 'SEN3'
+                        geoname = os.path.join(fname, 'xfdumanifest.xml')
+                        if geoname in zprod.namelist():
+                            gc = zprod.open(geoname)
+                            for line in gc:
+                                line_str = line.decode().strip()
+                                if line_str.startswith('<gml:posList>'):
+                                    clist = line_str[len('<gml:posList>'):line_str.index('</gml:posList>')].split()
+                                    coords = []
+                                    for i in range(0, len(clist), 2):
+                                        coord_here = (float(clist[i + 1]), float(clist[i]))
+                                        coords.append(coord_here)
+                                    polygon_image = Polygon(coords)  # create polygon
+                                    if point_site.within(polygon_image):
+                                        flag_location = 1
+                                    else:
+                                        flag_location = 0
+                            gc.close()
+
                 if flag_location == -1:
                     if args.verbose:
-                        print(f'Product: {prod} is not valid (SEN3, Level 1B or Baltic)')
-                    continue
+                        print(f'Product is not valid (SEN3, Level 1B or Baltic)')
                 if flag_location == 0:
                     if args.verbose:
-                        print(f'Product {prod} does not contain site location: {site}')
-                    continue
+                        print(f'Product does not contain site location: {site}')
                 if flag_location == 1:
                     if args.verbose:
-                        print(f'Trimming product: {prod} for site: {site}')
-                    prod_output = trimtool.make_trim(s, n, w, e, path_prod, None, False, out_dir_site, args.verbose)
-                    sval = path_prod + ';' + os.path.join(out_dir_site, prod_output)
-                    res_list.append(sval)
+                        print(f'Trimming product for site: {site}...')
+                    if iszipped:
+                        # path_temporal = '/mnt/c/data_luis/octac_work/bal_evolution/examples/unzip_folder'
+                        with zp.ZipFile(path_prod, 'r') as zprod:
+                            if args.verbose:
+                                print(f'Unziping to: {unzip_path}')
+                            zprod.extractall(path=unzip_path)
+                        path_prod_u = os.path.join(unzip_path, path_prod.split('/')[-1][0:-3] + 'SEN3')
+                        print(path_prod_u, os.path.exists(path_prod_u))
+                        prod_output = trimtool.make_trim(s, n, w, e, path_prod, None, False, out_dir_site, args.verbose)
+                        sval = path_prod + ';' + os.path.join(out_dir_site, prod_output)
+                        res_list.append(sval)
+                    else:
+                        prod_output = trimtool.make_trim(s, n, w, e, path_prod, None, False, out_dir_site, args.verbose)
+                        sval = path_prod + ';' + os.path.join(out_dir_site, prod_output)
+                        res_list.append(sval)
+
+                if args.verbose:
+                    print('-------------------------------------------------------------------')
 
     if args.createlist:
         file_list = os.path.join(out_dir_site, 'TrimList.txt')
@@ -150,7 +202,13 @@ def main():
             for row in res_list:
                 f.write(row)
                 f.write('\n')
-
+    if os.path.exists(unzip_path) and os.path.isdir(unzip_path):
+        if args.verbose:
+            print(f'Deleting temporary products in unzip folder: {unzip_path}')
+        for folder in os.listdir(unzip_path):
+            delete_folder(os.path.join(unzip_path,folder))
+        if args.verbose:
+            print('-------------------------------------------------------------------')
     print(f'COMPLETED. Trimmed files: {len(res_list)}')
 
 
@@ -180,6 +238,12 @@ def find_row_column_from_lat_lon(latArray, lonArray, in_situ_lat, in_situ_lon):
         r = -1
         c = -1
     return r, c
+
+
+def delete_folder(path_folder):
+    for f in os.listdir(path_folder):
+        os.remove(os.path.join(path_folder,f))
+    os.rmdir(path_folder)
 
 
 # Press the green button in the gutter to run the script.
